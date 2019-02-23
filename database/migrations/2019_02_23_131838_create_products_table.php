3c7b3bea342f0f4613dc2bb125818830e52590e3
Matthew <matthew@greyhoundit.co.uk>
2019-02-23 13:42:25 +0000

    init comit

---
+++ database/migrations/2019_02_23_131838_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned()->index();
+            $table->string('name', 255);
+            $table->text('description');
+            $table->tinyInteger('price');
+            $table->string('imageurl');
+            $table->string('slug',255);
+            $table->string('part_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
